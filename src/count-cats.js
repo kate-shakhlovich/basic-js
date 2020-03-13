342d848c35dae7f9872c5f21e147d6f871fce1e5
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-14 00:55:05 +0300

    feat: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) { 
+  const cats = "^^";
+  let count = 0;
+  for(let i = 0; i < matrix.length; i++) {
+    const row = matrix[i]; 
+    for(let j = 0; j < row.length; j++) { 
+      if(row[j] === cats) { 
+        count++; 
+      }
+    }
+  }
+  return count;
+}
