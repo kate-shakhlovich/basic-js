3090381f6bfe488f54ad5615e487b3f10ef17f2c
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-17 18:14:11 +0300

    feat: add hanoi tower and what season

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     const turns = Math.pow(2, disksNumber) - 1;
-    const seconds = turns / turnsSpeed * 3600;
+    const seconds = turns / (turnsSpeed / 3600);
     return { turns, seconds };
 }
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,24 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!isValidDate(date)) {
+    throw new Error();
+  }
+
+  const month = date.getMonth();
+  if (month === 11 || month === 0 || month === 1) {
+    return "winter";
+  } if (month === 2 || month === 3 || month === 4) {
+    return "spring";
+  } if (month === 5 || month === 6 || month === 7) {
+    return "summer";
+  } 
+  return "autumn";
 };
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
