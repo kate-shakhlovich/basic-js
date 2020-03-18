007a7b4ce15b28952d273733b0c9890a70c0d119
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-18 23:41:29 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || !sampleActivity) return false;
+  const sampleActivityValue = parseFloat(sampleActivity);
+  if (isNaN(sampleActivityValue) || sampleActivityValue <= 0 || sampleActivityValue > MODERN_ACTIVITY) return false;
+  const time = Math.log(MODERN_ACTIVITY / sampleActivityValue) / (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(time);
 };
