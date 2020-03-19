4b938dfc187fb6c7f43e5665e38fab8a0e2005f7
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-19 16:50:19 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+    separator = separator || '+';
+    additionSeparator = additionSeparator || '|';
+    addition = addition === undefined ? '' : addition;
+    str += repeatWithSeparator(addition, additionSeparator, additionRepeatTimes);
+    return repeatWithSeparator(str, separator, repeatTimes);
 };
-  
+
+function repeatWithSeparator(str, separator, repeatTimes) {
+    repeatTimes = repeatTimes || 1;
+    let resultString = "";
+    for (let i = 0; i < repeatTimes; i++) {
+        resultString += str;
+        if (i < repeatTimes - 1) {
+            resultString += separator;
+        }
+    }
+    return resultString;
+}
